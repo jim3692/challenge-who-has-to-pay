16f23912d9f6479ecd78538a9887715209cd0d34
jim3692 <jim3692@gmail.com>
2018-01-30 14:56:20 +0200

    Under construction - v0.1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+    public:
+        Student *LentTo;
+};
+
+int main()
+{
+    Student *Classroom[20];
+
+    for (Student *s : Classroom)
+    {
+
+    }
+
+    return 0;
+}
