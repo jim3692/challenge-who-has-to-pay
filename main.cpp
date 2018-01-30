4b1d87d43450b615d485726d14e4bd6fae4c3698
jim3692 <jim3692@gmail.com>
2018-01-30 17:24:20 +0200

    Hope it's done - v0.9

--- main.cpp
+++ main.cpp
@@ -1,20 +1,66 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
+
+const int StudentCount = 20;
+
+int A = 0x41;
 
 class Student
 {
     public:
-        Student *LentTo;
+        Student *lentTo;
+        string Name;
+        Student(string name) : Name(name) { }
+        void LentTo(Student *_lentTo) { lentTo = _lentTo; cout << (*this).Name << "->" << ((*this).lentTo == this ? "'None'" : (*_lentTo).Name) << endl; }
 };
 
 int main()
 {
-    Student *Classroom[20];
+    Student *Classroom[StudentCount];
 
-    for (Student *s : Classroom)
+    // Initialize Students
+    for (int i = 0; i < StudentCount; i++)
     {
+        char _str[1];
+        _str[0] = i + A;
+        Classroom[i] = new Student(_str);
+    }
 
+    // Define who lent to whom
+    for (int i = 0; i < StudentCount; i++)
+    {
+        int lend = (rand() % StudentCount);
+        (*Classroom[i]).LentTo(Classroom[lend]);
+    }
+
+    cout << endl; // Divide outputs
+
+    // Calculate who owes to whom
+    for (int i = 0; i < StudentCount; i++)
+    {
+        Student *current = Classroom[i];
+
+        while (true)
+        {
+            if (current == nullptr) break;;
+            if ((*current).lentTo == nullptr) break;
+            if ((*current).lentTo == current) { (*current).lentTo = nullptr; break; }
+
+            current = (*current).lentTo;
+        }
+
+        (*Classroom[i]).lentTo = current;
+    }
+
+    // Announce results
+    for (int i = 0; i < StudentCount; i++)
+    {
+        Student *current = Classroom[i];
+        if ((*current).lentTo == nullptr) continue;
+
+        cout << (*(*current).lentTo).Name << " owes to " << (*current).Name << endl;
     }
 
     return 0;
